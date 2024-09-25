93e8e1980266f7301617d39d385818c2470245ce
SerapisGod <108385357+SerapisGod@users.noreply.github.com>
2024-09-24 21:50:08 -0600

    Completed Assignment 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h1>Matthew Slepko</h1>
+            <Link href="https://github.com/SerapisGod/cprg306-assignments">My Git Repo</Link>
+        </div>
+    );
+};
+export default StudentInfo; 
